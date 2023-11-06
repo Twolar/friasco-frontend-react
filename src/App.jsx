f1b08951be5011fe4a6a4ae92e722bdc25b6272e
“Taylor <t4ybennett@gmail.com>
2023-11-06 18:42:50 +0000

    Getting back to base template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import Topbar from "./scenes/global/TopBar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router";
+import Sidebar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/users" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
