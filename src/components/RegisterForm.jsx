f17041fbfd25d2b9f2bac7fdcf653006af7d4c22
“Taylor <t4ybennett@gmail.com>
2023-11-07 13:11:28 +0000

    Add register form

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,228 @@
+import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { tokens } from "../theme";
+import AuthContext from "../context/AuthProvider";
+import axios from "../api/axios";
+const REGISTER_URL = "/register";
+
+const CreateUserForm = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const errRef = useRef();
+  const { setAuth } = useContext(AuthContext);
+
+  const [errMessage, setErrMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const handleFormSubmit = async (formData, { resetForm }) => {
+    try {
+      const response = await axios.post(
+        REGISTER_URL,
+        JSON.stringify({
+          username: formData.username,
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          email: formData.email,
+          role: "User",
+          password: formData.password,
+          confirmPassword: formData.confirmPassword,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      setAuth({
+        email: formData.email,
+        password: formData.password,
+        token: response.data.token,
+      });
+      setSuccess(true);
+    } catch (error) {
+      console.log(response);
+      if (!error?.response) {
+        setErrMessage("No Server Response");
+      } else if (error?.response?.status === 400) {
+        setErrMessage(error?.response?.data?.Errors?.Exception[0]);
+      } else {
+        setErrMessage("Register Failed");
+      }
+
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <Box width="100%">
+      {success ? (
+        <section>
+          <Typography
+            variant="p"
+            color={colors.grey[100]}
+            fontWeight="bold"
+            sx={{ mb: "5px" }}
+          >
+            Registration successful!
+          </Typography>
+        </section>
+      ) : (
+        <>
+          <section>
+            <Typography
+              variant="p"
+              ref={errRef}
+              className={errMessage ? "errorMessage" : "hidden"}
+              aria-live="assertive"
+            >
+              {errMessage}
+            </Typography>
+          </section>
+
+          <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={initialValues}
+            validationSchema={checkoutSchema}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Box
+                  display="grid"
+                  gap="30px"
+                  gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                  sx={{
+                    "& > div": {
+                      gridColumn: isNonMobile ? undefined : "span 4",
+                    },
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="First Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.firstName}
+                    name="firstName"
+                    error={!!touched.firstName && !!errors.firstName}
+                    helperText={touched.firstName && errors.firstName}
+                    sx={{ gridColumn: "span 2" }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Last Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.lastName}
+                    name="lastName"
+                    error={!!touched.lastName && !!errors.lastName}
+                    helperText={touched.lastName && errors.lastName}
+                    sx={{ gridColumn: "span 2" }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    name="email"
+                    error={!!touched.email && !!errors.email}
+                    helperText={touched.email && errors.email}
+                    sx={{ gridColumn: "span 4" }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Username"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.username}
+                    name="username"
+                    error={!!touched.username && !!errors.username}
+                    helperText={touched.username && errors.username}
+                    sx={{ gridColumn: "span 4" }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="password"
+                    label="Password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                    name="password"
+                    error={!!touched.password && !!errors.password}
+                    helperText={touched.password && errors.password}
+                    sx={{ gridColumn: "span 2" }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="password"
+                    label="Confirm Password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.confirmPassword}
+                    name="confirmPassword"
+                    error={
+                      !!touched.confirmPassword && !!errors.confirmPassword
+                    }
+                    helperText={
+                      touched.confirmPassword && errors.confirmPassword
+                    }
+                    sx={{ gridColumn: "span 2" }}
+                  />
+                </Box>
+                <Box display="flex" justifyContent="end" mt="20px">
+                  <Button type="submit" color="secondary" variant="contained">
+                    SUBMIT
+                  </Button>
+                </Box>
+              </form>
+            )}
+          </Formik>
+        </>
+      )}
+    </Box>
+  );
+};
+
+const checkoutSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  username: yup.string().required("required"),
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  password: yup.string().required("required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("required"),
+});
+
+const initialValues = {
+  username: "string",
+  firstName: "string",
+  lastName: "string",
+  email: "user@example.com",
+  role: "User",
+  password: "string",
+  confirmPassword: "string",
+};
+
+export default RegisterForm;
--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -1,12 +1,16 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import LoginForm from "../../components/LoginForm";
+import RegisterForm from "../../components/RegisterForm";
 
 const Login = () => {
   return (
     <Box m="0px 20px">
       <Header title="LOGIN" subtitle="Welcome to the ReactWebApp!"></Header>
       <LoginForm />
+
+      <Header title="REGISTER" subtitle="Welcome to the ReactWebApp!"></Header>
+      <RegisterForm></RegisterForm>
     </Box>
   );
 };
