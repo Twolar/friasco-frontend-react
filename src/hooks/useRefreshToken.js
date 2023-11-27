ded8fe3109bfcd169aea5a95ad8578b4cf86a5c3
“Taylor <t4ybennett@gmail.com>
2023-11-27 21:27:47 +0000

    Refactor for not needing access token in request

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -5,16 +5,13 @@ const useRefreshToken = () => {
   const { auth, setAuth } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.post("/Auth/Refresh",
-      JSON.stringify({
-        token: auth.token,
-      }),
-      {
-        headers: { "Content-Type": "application/json" },
-        withCredentials: true,
-      }
-    );
+    const response = await axios.get("/Auth/Refresh", {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    });
     setAuth((prev) => {
+      console.log("Prev: " + prev);
+      console.log("New Token: " + response.data.token);
       return { ...prev, token: response.data.token };
     });
     return response.data.token;
