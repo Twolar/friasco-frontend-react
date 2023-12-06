08917340e1857119aa9b6a608319acdd5a68c20f
“Taylor <t4ybennett@gmail.com>
2023-12-06 20:19:36 +0000

    Init of admin user management grid

---
+++ src/components/ADUserGrid.jsx
@@ -0,0 +1,164 @@
+import { DataGrid } from "@mui/x-data-grid";
+import { useTheme, Box, Typography, IconButton } from "@mui/material";
+import { tokens } from "../theme";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { ApiUrlEnum, SceneRoutePathEnum, UserRoleEnum } from "../helpers/enums";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const ADUserGrid = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const navigate = useNavigate();
+  const axiosPrivate = useAxiosPrivate();
+
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get(ApiUrlEnum.Users, {
+          signal: controller.signal,
+        });
+        isMounted && setUsers(response.data);
+
+        console.log(response.data);
+      } catch (err) {
+        console.error(err);
+        navigate(SceneRoutePathEnum.Login, {
+          state: { from: location },
+          replace: true,
+        });
+      }
+    };
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "username",
+      headerName: "Username",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 1,
+      renderCell: ({ row: { role } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 0"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+                role === UserRoleEnum.SuperAdmin && colors.redAccent[600] ||
+                role === UserRoleEnum.Admin && colors.blueAccent[600] ||
+              role === UserRoleEnum.User && colors.greenAccent[600]
+            }
+            borderRadius="4px"
+          >
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {role}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "",
+      headerName: "Actions",
+      renderCell: (params) => (
+        <>
+          <IconButton
+            onClick={() => deleteUserRow(params.id)}
+            style={{
+              color: colors.redAccent[300],
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <Box
+      width="100%"
+      sx={{
+        "& .MuiDataGrid-root": {
+          border: "none",
+        },
+        "& .MuiDataGrid-cell": {
+          borderBottom: "none",
+        },
+        "& .name-column--cell": {
+          color: colors.greenAccent[300],
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: colors.blueAccent[700],
+          borderBottom: "none",
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backgroundColor: colors.primary[400],
+        },
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: colors.blueAccent[700],
+        },
+        "& .MuiCheckbox-root": {
+          color: `${colors.greenAccent[200]} !important`,
+        },
+        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+          color: `${colors.grey[100]} !important`,
+        },
+      }}
+    >
+      <DataGrid
+        rows={users}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[10]}
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+};
+
+export default ADUserGrid;
--- src/scenes/admin/userManagment/index.jsx
+++ src/scenes/admin/userManagment/index.jsx
@@ -1,65 +1,18 @@
 import { Box } from "@mui/material";
 import Header from "../../../components/Header";
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
-import { ApiUrlEnum, SceneRoutePathEnum } from "../../../helpers/enums";
+import ADUserGrid from "../../../components/ADUserGrid";
 
-const Users = () => {
-  const [users, setUsers] = useState();
-  const axiosPrivate = useAxiosPrivate();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    let isMounted = true;
-    const controller = new AbortController();
-
-    const getUsers = async () => {
-      try {
-        const response = await axiosPrivate.get(ApiUrlEnum.Users, {
-          signal: controller.signal,
-        });
-        isMounted && setUsers(response.data);
-      } catch (err) {
-        console.error(err);
-        navigate(SceneRoutePathEnum.Login, {
-          state: { from: location },
-          replace: true,
-        });
-      }
-    };
-
-    getUsers();
-
-    return () => {
-      isMounted = false;
-      controller.abort();
-    };
-  }, []);
-
+const ADUserManagment = () => {
   return (
     <Box m="0px 20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="USER MANAGEMENT" subtitle=""></Header>
+        <Header title="USER MANAGEMENT" subtitle="Manage system users here!" />
       </Box>
-
-      <Box display="flex">
-        <article>
-          <h2>List</h2>
-          {users?.length ? (
-            <ul>
-              {users.map((user, i) => (
-                <li key={i}>{user?.username}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>No users to display</p>
-          )}
-        </article>
+      <Box mt="15px">
+        <ADUserGrid />
       </Box>
     </Box>
   );
 };
 
-export default Users;
+export default ADUserManagment;
